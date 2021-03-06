90b260b12b2777efb2806e8c246436afe0ec8141
Nick Carver <nicholascarver2@outlook.com>
2021-03-06 09:21:16 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from "react"
+import { Route, Link } from "react-router-dom"
+import picture1 from './picture1.jpg';
+import picture2 from './picture2.jpg';
+import picture3 from './picture3.jpg';
+import picture4 from './picture4.jpg';
+import picture5 from './picture5.jpg';
+import picture6 from './picture6.jpg';
+import picture7 from './picture7.jpg';
 import './App.css';
 
-function App() {
+export const MomsBirthday = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <Route exact path="/">
+        <div className="App">
+          <header className="App-header">
+            <div className="photos">
+              <img src={picture1} className="picture1-4 photo" alt="logo" />
+              <img src={picture2} className="picture2-5 photo" alt="logo" />
+              <img src={picture3} className="picture3-6 photo" alt="logo" />
+              <img src={picture4} className="picture1-4 photo" alt="logo" />
+              <img src={picture5} className="picture2-5 photo" alt="logo" />
+              <img src={picture6} className="picture3-6 photo" alt="logo" />
+            </div>
+            <div className="text">
+              <p>
+                Happy Birthday
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+              <Link className="App-link" to="/info">MOM</Link>
+            </div>
+          </header>
+        </div>
+      </Route>
+      <Route exact path="/info">
+        <div className="App">
+          <header className="App-header">
+            <div className="photos">
+              <img src={picture7} className="picture7 photo7" alt="logo" />
+            </div>
+            <div className="text">
+              <p>
+                I LOVE YOU
+              </p>
+              <Link className="App-link" to="/">Back</Link>
+            </div>
+          </header>
+        </div>
+      </Route>
+    </>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { MomsBirthday } from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <MomsBirthday />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
